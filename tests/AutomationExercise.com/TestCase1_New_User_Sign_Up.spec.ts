baeecad10dbb618320308997ed5d71acc62ad6bc
Irish Ann Morales <iamorales.main@gmail.com>
2025-04-28 19:26:10 +0800

    To continue Test Case New User Registration

--- tests/AutomationExercise.com/TestCase1_New_User_Sign_Up.spec.ts
+++ tests/AutomationExercise.com/TestCase1_New_User_Sign_Up.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test';
 
+//Verify 
 test('Verify if able to register successfully', async  ({ page }) => {
     //Go to homepage
     await page.goto('https://automationexercise.com/')
@@ -12,4 +13,17 @@ test('Verify if able to register successfully', async  ({ page }) => {
 
     //Assert the new URL is correct
     await expect(page).toHaveURL('https://automationexercise.com/login');
+
+    // Wait for Name and Email input fields to be visible 
+    await page.locator('[data-qa="signup-name"]').waitFor();
+    await page.locator('[data-qa="signup-email"]').waitFor();
+
+    //Input name and email address
+    await page.locator('[data-qa="signup-name"]').fill('irishann')
+    await page.locator('[data-qa="signup-email"]').fill('iamorales.main@gmail.com')
+    await page.locator('[data-qa="signup-button"]').click()
+
+    //Redirect to Sign Up page
+    await expect(page).toHaveURL('https://automationexercise.com/signup');
 })
+
