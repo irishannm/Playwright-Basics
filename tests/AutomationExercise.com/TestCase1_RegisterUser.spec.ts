56055dca017df9083fa017c64acfd41a3e166ef0
Irish Ann Morales <iamorales.main@gmail.com>
2025-04-29 08:32:08 +0800

    Added json file, globals for test data

---
+++ tests/AutomationExercise.com/TestCase1_RegisterUser.spec.ts
@@ -0,0 +1,35 @@
+import {test, expect} from '@playwright/test';
+import testData from '../testData/testData.json';
+
+test('Verify if able to register successfully', async  ({ page }) => {
+
+    //Test Data
+    const {name, email} = testData;
+
+    //2. Navigate to URL
+    await page.goto('https://automationexercise.com')
+
+    //3. Verify that home page is visible
+    await expect(page).toHaveURL('https://automationexercise.com/')
+
+    //4. Click on Signup/ Login button
+    await page.getByRole('link',{name: ' Signup / Login'}).click()
+
+    //5. Verify New User Signup! is visible
+    await expect(page.getByRole('heading',{ name: 'New  User Signup!'})).toBeVisible();
+
+    //6. Enter name and email address
+    await page.locator('[data-qa="signup-name"]').fill(name)
+    await page.locator('[data-qa="signup-email"]').fill(email)
+    await page.locator('[data-qa="signup-button"]').click()
+
+    //7. Cick 'Signup' button //- That uses a case-insensitive RegEx 
+    await page.getByRole('button',{ name: /signup/i }).click()
+
+    //8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
+    await expect(page.getByText(/enter account information/i)).toBeVisible()
+
+    
+})
+
+
