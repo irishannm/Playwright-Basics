239c62553aee44112ef7284a5b81bf115e32de7d
Irish Ann Morales <iamorales.main@gmail.com>
2025-05-02 18:58:49 +0800

    Started testcase2

--- tests/AutomationExercise.com/TestCase2_LoginWithValidCredentials.spec.ts
+++ tests/AutomationExercise.com/TestCase2_LoginWithValidCredentials.spec.ts
@@ -3,7 +3,11 @@ import {test, expect} from '@playwright/test';
 test('Verify if able to login with correct email and password', async  ({ page }) => {
     // 1. Launch browser
     // 2. Navigate to url 'http://automationexercise.com'
+    await page.goto("http://automationexercise.com")
+
     // 3. Verify that home page is visible successfully
+    await expect(page).toHaveURL("http://automationexercise.com")
+    
     // 4. Click on 'Signup / Login' button
     // 5. Verify 'Login to your account' is visible
     // 6. Enter correct email address and password
